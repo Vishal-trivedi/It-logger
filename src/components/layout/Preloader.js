1a95f102c122496760ee0ce57ec3086fb34885b9
126-stack <1234vishaltrvedi1234@gmail.com>
2020-05-02 02:11:01 +0530

    first commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Preloader = () => {
+    return (
+        <div className="progess blue lighten-4">
+         <div className="indeterminate blue"></div>
+            
+        </div>
+    )
+}
+
+export default Preloader
